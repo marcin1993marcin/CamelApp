171498d8f9720e616ffed517403fcff5c554b08e
Marcin Bronecki <marcin1993marcin@gmail.com>
2016-10-24 13:11:46 +0200

    Update Application class

--- src/main/java/com/app/camel/Application.java
+++ src/main/java/com/app/camel/Application.java
@@ -1,6 +1,6 @@
 package com.app.camel;
 
-import com.app.camel.configuration.Config;
+import com.app.camel.configuration.DatabaseConfiguration;
 import com.app.camel.routes.ProjectRoute;
 import com.app.camel.routes.UserRoute;
 import org.apache.camel.CamelContext;
@@ -9,7 +9,7 @@ import org.apache.camel.impl.DefaultCamelContext;
 public class Application {
     public static void main(String[] args) throws Exception {
 
-        Class.forName(Config.DRIVER).newInstance();
+        Class.forName(DatabaseConfiguration.DRIVER).newInstance();
         CamelContext context = new DefaultCamelContext();
 
         UserRoute restfulRoute = new UserRoute();
