357dd33b7b8d82579fe4eaef9d7ea1527e2bba26
Marcin Bronecki <marcin1993marcin@gmail.com>
2016-10-24 12:34:57 +0200

    Refactor Application class

--- src/main/java/com/app/camel/Application.java
+++ src/main/java/com/app/camel/Application.java
@@ -1,5 +1,8 @@
 package com.app.camel;
 
+import com.app.camel.configuration.Config;
+import com.app.camel.routes.ProjectRoute;
+import com.app.camel.routes.UserRoute;
 import org.apache.camel.CamelContext;
 import org.apache.camel.impl.DefaultCamelContext;
 
@@ -7,12 +10,14 @@ public class Application {
     public static void main(String[] args) throws Exception {
 
         Class.forName(Config.DRIVER).newInstance();
-
         CamelContext context = new DefaultCamelContext();
+
         UserRoute restfulRoute = new UserRoute();
         ProjectRoute projectRoute= new ProjectRoute();
+
         context.addRoutes(restfulRoute);
         context.addRoutes(projectRoute);
+
         context.start();
         Thread.sleep(5000000);
         context.stop();
