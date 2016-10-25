e8938c1b11acf8a7d73a6d574b9a3060a234e7a2
Marcin Bronecki <marcin1993marcin@gmail.com>
2016-10-25 12:23:10 +0200

    Delete Database and Flyway configuration class

--- src/main/java/com/app/camel/configuration/FlywayConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.app.camel.configuration;
-
-public class FlywayConfiguration {
-
-    public static final String SCHEMA_NAME = "library";
-    public static final String URL_FOR_FLYWAY_CONFIGURATION = "jdbc:mysql://localhost:3306/?serverTimezone=UTC";
-
-}
