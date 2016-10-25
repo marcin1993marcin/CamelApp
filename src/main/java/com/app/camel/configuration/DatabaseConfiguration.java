df21bf666bec33521c35ae1f338b6b2e6a545222
Marcin Bronecki <marcin1993marcin@gmail.com>
2016-10-25 10:07:45 +0200

    Update DatabaseConfiguration

--- src/main/java/com/app/camel/configuration/DatabaseConfiguration.java
+++ src/main/java/com/app/camel/configuration/DatabaseConfiguration.java
@@ -2,6 +2,7 @@ package com.app.camel.configuration;
 
 public class DatabaseConfiguration {
 
+    public static final String URL_FOR_FLYWAY_CONFIGURATION = "jdbc:mysql://localhost:3306/?serverTimezone=UTC";
     public static final String URL = "jdbc:mysql://localhost:3306/library?serverTimezone=UTC";
     public static final String USER = "root";
     public static final String PASSWORD = "root";
