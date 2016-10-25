6c051ec2f88e028a0419c9b48b536e0e4a325289
Marcin Bronecki <marcin1993marcin@gmail.com>
2016-10-25 09:54:34 +0200

    Update create_tables script

--- src/main/resources/db/migration/V1_0_1__create_tables.sql
+++ src/main/resources/db/migration/V1_0_1__create_tables.sql
@@ -2,7 +2,7 @@
 -- -----------------------------------------------------
 -- Table `library`.`project`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `project` (
+CREATE TABLE IF NOT EXISTS `library`.`project` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `project_name` VARCHAR(200) NOT NULL,
   PRIMARY KEY (`id`))
@@ -14,7 +14,7 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `library`.`user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE IF NOT EXISTS `library`.`user` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(255) NOT NULL,
   `last_name` VARCHAR(255) NOT NULL,
@@ -29,7 +29,7 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `library`.`user_projects`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `user_projects` (
+CREATE TABLE IF NOT EXISTS `library`.`user_projects` (
   `Users_id` INT(11) NOT NULL,
   `Projects_id` INT(11) NOT NULL,
   PRIMARY KEY (`Users_id`, `Projects_id`),
