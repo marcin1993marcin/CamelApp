b17bc7f5272fdc6b2ad1c930b3b0c8a8375669fd
Marcin Bronecki <marcin1993marcin@gmail.com>
2016-10-25 10:32:44 +0200

    Update Application class

--- src/main/java/com/app/camel/Application.java
+++ src/main/java/com/app/camel/Application.java
@@ -1,6 +1,7 @@
 package com.app.camel;
 
 import com.app.camel.configuration.DatabaseConfiguration;
+import com.app.camel.configuration.FlywayConfiguration;
 import com.app.camel.routes.ProjectRoute;
 import com.app.camel.routes.UserRoute;
 import org.apache.camel.CamelContext;
@@ -16,7 +17,7 @@ public class Application {
         Flyway flyway = new Flyway();
         flyway.setBaselineOnMigrate(true);
         flyway.setDataSource(DatabaseConfiguration.URL_FOR_FLYWAY_CONFIGURATION, DatabaseConfiguration.USER, DatabaseConfiguration.PASSWORD);
-        flyway.setSchemas("library");
+        flyway.setSchemas(FlywayConfiguration.SCHEMA_NAME);
         flyway.migrate();
 
         UserRoute restfulRoute = new UserRoute();
