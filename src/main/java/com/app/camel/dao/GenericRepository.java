a8527aaa020a3f598bda85a3c3f07985ada8a89e
Marcin Bronecki <marcin1993marcin@gmail.com>
2016-10-26 09:33:24 +0200

    Add throws signature in GenericRepository interface

--- src/main/java/com/app/camel/dao/GenericRepository.java
+++ src/main/java/com/app/camel/dao/GenericRepository.java
@@ -6,10 +6,10 @@ import java.util.Optional;
 
 public interface GenericRepository<ID, T> {
 
-    Optional<T> get(ID id);
+    Optional<T> get(ID id) throws SQLException;
     Collection<T> getAll() throws SQLException;
-    boolean update(T entity);
-    boolean insert(T entity);
-    boolean delete(ID id);
-    boolean deleteAll();
+    boolean update(T entity) throws SQLException;
+    boolean insert(T entity) throws SQLException;
+    boolean delete(ID id) throws SQLException;
+    boolean deleteAll() throws SQLException;
 }
