46dd691f9134bc3adf78e0d027d4bd3c25e49de0
Marcin Bronecki <marcin1993marcin@gmail.com>
2016-10-26 09:06:20 +0200

    Change return type of get method

--- src/main/java/com/app/camel/dao/GenericRepository.java
+++ src/main/java/com/app/camel/dao/GenericRepository.java
@@ -1,11 +1,13 @@
 package com.app.camel.dao;
 
+import java.sql.SQLException;
 import java.util.Collection;
+import java.util.Optional;
 
 public interface GenericRepository<ID, T> {
 
-    T get(ID id);
-    Collection<T> getAll();
+    Optional<T> get(ID id);
+    Collection<T> getAll() throws SQLException;
     boolean update(T entity);
     boolean insert(T entity);
     boolean delete(ID id);
