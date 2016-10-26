8a8f64c6f27323943b5a92d2315a4e1a06fa0514
Marcin Bronecki <marcin1993marcin@gmail.com>
2016-10-26 09:34:01 +0200

    Add throws signature in UserRepository interface

--- src/main/java/com/app/camel/dao/UserRepository.java
+++ src/main/java/com/app/camel/dao/UserRepository.java
@@ -2,8 +2,10 @@ package com.app.camel.dao;
 
 import com.app.camel.model.tables.records.UserRecord;
 
+import java.sql.SQLException;
+
 public interface UserRepository extends GenericRepository<Integer, UserRecord>{
 
-    String getAllUserWithProject();
+    String getAllUserWithProject() throws SQLException;
 
 }
