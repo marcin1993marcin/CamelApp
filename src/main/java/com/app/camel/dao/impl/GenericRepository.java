ec83ec26a20532209f882688c5dc6da07a8c2e1e
Marcin Bronecki <marcin1993marcin@gmail.com>
2016-10-27 10:25:08 +0200

    Update GenericRepository class

--- src/main/java/com/app/camel/dao/impl/GenericRepository.java
+++ src/main/java/com/app/camel/dao/impl/GenericRepository.java
@@ -1,7 +1,6 @@
 package com.app.camel.dao.impl;
 
 import com.app.camel.configuration.Configuration;
-import org.apache.log4j.Logger;
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
@@ -9,26 +8,19 @@ import org.jooq.impl.DSL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.function.Function;
 
 public class GenericRepository {
 
-    final static Logger logger = Logger.getLogger(GenericRepository.class);
-
-    protected DSLContext getDSLContext() {
+    protected <T> T executeQuery(Function<DSLContext, T> consumer) throws SQLException {
         try (Connection connection = DriverManager.getConnection(
                 Configuration.DATABASE_URL,
                 Configuration.DATABASE_USER,
                 Configuration.DATABASE_PASSWORD);
 
-             DSLContext dslContext = DSL.using(connection, SQLDialect.MYSQL)) {
+            DSLContext dslContext = DSL.using(connection, SQLDialect.MYSQL)) {
 
-            return dslContext;
-
-        } catch (SQLException sqle) {
-            sqle.printStackTrace();
-            logger.error("Error establishing a database connection");
+            return consumer.apply(dslContext);
         }
-
-        return null;
     }
 }
