20a8033c76e6ffa23d9a17425f9aa89236eaf21e
Marcin Bronecki <marcin1993marcin@gmail.com>
2016-10-28 08:59:42 +0200

    Update GenericRepository class

--- src/main/java/com/app/camel/dao/impl/GenericRepository.java
+++ src/main/java/com/app/camel/dao/impl/GenericRepository.java
@@ -1,8 +1,10 @@
 package com.app.camel.dao.impl;
 
 import com.app.camel.configuration.Configuration;
+import org.apache.log4j.Logger;
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
+import org.jooq.exception.DataAccessException;
 import org.jooq.impl.DSL;
 
 import java.sql.Connection;
@@ -12,15 +14,22 @@ import java.util.function.Function;
 
 public class GenericRepository {
 
-    protected <T> T executeQuery(Function<DSLContext, T> consumer) throws SQLException {
+    Logger LOGGER = Logger.getLogger(GenericRepository.class);
+
+    protected <T> T executeQuery(Function<DSLContext, T> consumer) {
         try (Connection connection = DriverManager.getConnection(
                 Configuration.DATABASE_URL,
                 Configuration.DATABASE_USER,
                 Configuration.DATABASE_PASSWORD);
 
-            DSLContext dslContext = DSL.using(connection, SQLDialect.MYSQL)) {
+             DSLContext dslContext = DSL.using(connection, SQLDialect.MYSQL)) {
 
             return consumer.apply(dslContext);
+        } catch (SQLException sqle) {
+
+            LOGGER.error("error getting data for", sqle);
+
+            throw new DataAccessException("error getting data for", sqle);
         }
     }
 }
