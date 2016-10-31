f64130b08e7113d5e9c40b10875300ae944ba8e0
Marcin Bronecki <marcin1993marcin@gmail.com>
2016-10-31 10:37:40 +0100

    Add JavaDoc in UserRepository interface

--- src/main/java/com/app/camel/dao/UserRepository.java
+++ src/main/java/com/app/camel/dao/UserRepository.java
@@ -4,8 +4,20 @@ import com.app.camel.model.tables.records.UserRecord;
 
 import java.sql.SQLException;
 
+/**
+ * Interface which execute simple CRUD operations.
+ * Extends GenericRepository and specify ID type as Integer
+ * and map return type of CRUD operations as ProjectRecord entity
+ *
+ * Inherit from GenericRepository the following methods:
+ *
+ * Optional<UserRecord> get(Integer id);
+ * Collection<UserRecord> getAll();
+ * boolean update(UserRecord entity);
+ * boolean insert(UserRecord entity);
+ * boolean delete(Integer id);
+ * boolean deleteAll();
+ */
 public interface UserRepository extends GenericRepository<Integer, UserRecord>{
-
-    String getAllUserWithProject() throws SQLException;
 
 }
