f5eab350751e1bb39806e99c0fd4a9d46ce77aaf
Marcin Bronecki <marcin1993marcin@gmail.com>
2016-10-31 10:29:44 +0100

    Add JavaDoc to ProjectRepository interface

--- src/main/java/com/app/camel/dao/ProjectRepository.java
+++ src/main/java/com/app/camel/dao/ProjectRepository.java
@@ -2,6 +2,20 @@ package com.app.camel.dao;
 
 import com.app.camel.model.tables.records.ProjectRecord;
 
+/**
+ * Interface which execute simple CRUD operations.
+ * Extends GenericRepository and specify ID type as Integer
+ * and map return type of CRUD operations as ProjectRecord entity
+ *
+ * Inherit from GenericRepository the following methods:
+ *
+ * Optional<ProjectRecord> get(Integer id);
+ * Collection<ProjectRecord> getAll();
+ * boolean update(ProjectRecord entity);
+ * boolean insert(ProjectRecord entity);
+ * boolean delete(Integer id);
+ * boolean deleteAll();
+ */
 public interface ProjectRepository extends GenericRepository<Integer, ProjectRecord> {
 
 }
