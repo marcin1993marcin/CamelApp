9a2d4d233725260c3b20e5b9a57ff23dcf629820
Marcin Bronecki <marcin1993marcin@gmail.com>
2016-10-31 12:49:34 +0100

    Add logs to ProjectRepositoryImpl class

--- src/main/java/com/app/camel/dao/impl/ProjectRepositoryImpl.java
+++ src/main/java/com/app/camel/dao/impl/ProjectRepositoryImpl.java
@@ -2,7 +2,9 @@ package com.app.camel.dao.impl;
 
 import com.app.camel.dao.ProjectRepository;
 import com.app.camel.model.tables.records.ProjectRecord;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
+import org.apache.log4j.Logger;
 import org.jooq.Record;
 import org.jooq.Result;
 
@@ -13,17 +15,44 @@ import static com.app.camel.model.tables.Project.PROJECT;
 
 public class ProjectRepositoryImpl extends GenericRepository implements ProjectRepository {
 
+    private static final Logger LOGGER = Logger.getLogger(ProjectRepositoryImpl.class);
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Optional<ProjectRecord> get(Integer id) {
 
-        return executeQuery(ctx -> Optional.ofNullable(
+        try {
+            Preconditions.checkNotNull(id);
+        } catch (NullPointerException ex) {
+            LOGGER.error("Getting project failed! Id cannot be null");
+            ex.printStackTrace();
+        }
+
+        LOGGER.info("Getting project with id: " + id);
+
+        Optional<ProjectRecord> projectRecord = executeQuery(ctx -> Optional.ofNullable(
                 ctx.selectFrom(PROJECT)
                         .where(PROJECT.ID.equal(id))
                         .fetchOne()));
+
+        if (projectRecord.isPresent()) {
+            LOGGER.info("Project with id: " + id + " fetched successfully");
+        } else {
+            LOGGER.info("Project with id: " + id + " not found");
+        }
+
+        return projectRecord;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Collection<ProjectRecord> getAll() {
+
+        LOGGER.info("Getting all projects from database");
 
         return executeQuery(ctx -> {
 
@@ -31,22 +60,39 @@ public class ProjectRepositoryImpl extends GenericRepository implements ProjectR
 
             Collection<ProjectRecord> projects = Lists.newArrayList();
 
-            for (Record r : result) {
+            result.forEach(r -> {
                 ProjectRecord project = new ProjectRecord(
                         r.getValue(PROJECT.ID),
                         r.getValue(PROJECT.PROJECT_NAME));
 
                 projects.add(project);
+            });
 
+            if (projects.size() > 0) {
+                LOGGER.info("Successfully fetched all projects from database");
+            } else {
+                LOGGER.info("Cannot get all projects. No projects found in database");
             }
 
             return projects;
         });
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean update(ProjectRecord project) {
+
+        try {
+            Preconditions.checkNotNull(project);
+            Preconditions.checkNotNull(project.getId());
+        } catch (NullPointerException ex) {
+            LOGGER.error("Updating project record failed! ProjectRecord or ProjectRecord id cannot be null");
+            ex.printStackTrace();
+        }
+
+        LOGGER.info("Updating project with id: " + project.getId());
 
         return executeQuery(ctx -> {
             int count = ctx.update(PROJECT)
@@ -54,13 +100,30 @@ public class ProjectRepositoryImpl extends GenericRepository implements ProjectR
                     .where(PROJECT.ID.eq(project.getId()))
                     .execute();
 
+            if (count > 0) {
+                LOGGER.info("Successfully updated project with id: " + project.getId());
+            } else {
+                LOGGER.info("Cannot update project with id: " + project.getId() + ". Project not found");
+            }
+
             return count > 0;
         });
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean insert(ProjectRecord project) {
+
+        try {
+            Preconditions.checkNotNull(project);
+        } catch (NullPointerException ex) {
+            LOGGER.error("Adding project record failed! ProjectRecord cannot be null");
+            ex.printStackTrace();
+        }
+
+        LOGGER.info("Adding project with id: " + project.getId());
 
         return executeQuery(ctx -> {
             com.app.camel.model.tables.Project p = com.app.camel.model.tables.Project.PROJECT;
@@ -69,38 +132,62 @@ public class ProjectRepositoryImpl extends GenericRepository implements ProjectR
                     .set(p.PROJECT_NAME, project.getProjectName())
                     .execute();
 
+            if (count > 0) {
+                LOGGER.info("Successfully added project with id: " + project.getId());
+            } else {
+                LOGGER.info("Cannot add project with id: " + project.getId());
+            }
+
             return count > 0;
         });
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean delete(Integer id) {
 
-        // logujemy, ze usuwamy
+        try {
+            Preconditions.checkNotNull(id);
+        } catch (NullPointerException ex) {
+            LOGGER.error("Deleting project failed! Id cannot be null");
+            ex.printStackTrace();
+        }
 
-        boolean result =  executeQuery(ctx -> {
+        LOGGER.info("Deleting project with id: " + id);
+
+        return executeQuery(ctx -> {
             int count = ctx.delete(PROJECT).where(PROJECT.ID.eq(id)).execute();
+
+            if (count > 0) {
+                LOGGER.info("Project with id: " + id + " deleted successfully");
+            } else {
+                LOGGER.info("Project not found! Cannot delete project with id: " + id);
+            }
 
             return count > 0;
         });
-
-        // logujemy status: result
-
-        return result;
     }
 
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean deleteAll() {
+
+        LOGGER.info("Deleting all projects");
 
         return executeQuery(ctx -> {
             Result<Record> result = ctx.select().from(PROJECT).fetch();
 
-            int count = 0;
-            for (Record r : result) {
+            int count = ctx.delete(PROJECT).execute();
 
-                count = ctx.delete(PROJECT).where(PROJECT.ID.eq(r.getValue(PROJECT.ID))).execute();
+            if (count > 0) {
+                LOGGER.info("All projects deleted successfully");
+            } else {
+                LOGGER.info("Cannot delete projects. Database is already empty");
             }
 
             return count > 0;
