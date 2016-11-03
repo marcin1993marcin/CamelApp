7469ce088d980436a04557c3010265548c6426e0
Maksym Osmuk <maxim.osmuk@gmail.com>
2016-11-03 11:15:18 +0100

    Add Skill class

---
+++ src/main/java/com/app/camel/dto/Skill.java
@@ -0,0 +1,15 @@
+package com.app.camel.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Builder
+public class Skill {
+
+    private Long id;
+    private String name;
+    private Integer parentId;
+}
