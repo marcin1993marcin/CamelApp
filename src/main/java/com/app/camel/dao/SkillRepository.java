2402106305694d887c7db59b4e5837d7d109f364
Maksym Osmuk <maxim.osmuk@gmail.com>
2016-11-03 15:37:02 +0100

    Add javadoc for SkillRepository interface

--- src/main/java/com/app/camel/dao/SkillRepository.java
+++ src/main/java/com/app/camel/dao/SkillRepository.java
@@ -2,5 +2,19 @@ package com.app.camel.dao;
 
 import com.app.camel.model.tables.records.SkillRecord;
 
+/**
+ * Interface which execute simple CRUD operations.
+ * Extends GenericRepository and specify ID type as Integer
+ * and map return type of CRUD operations as SkillRecord entity
+ *
+ * Inherit from GenericRepository the following methods:
+ *
+ * Optional<SkillRecord> get(Integer id);
+ * Collection<SkillRecord> getAll();
+ * boolean update(SkillRecord entity);
+ * boolean insert(SkillRecord entity);
+ * boolean delete(Integer id);
+ * boolean deleteAll();
+ */
 public interface SkillRepository extends GenericRepository<Integer, SkillRecord> {
 }
