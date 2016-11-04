88c83ab5617d8e29c25c136afe43522625b4bf4a
gfuta <grzegorz.futa@gmail.com>
2016-11-04 09:02:06 +0100

    SLF4J in application

--- src/main/java/com/app/camel/Application.java
+++ src/main/java/com/app/camel/Application.java
@@ -1,22 +1,23 @@
 package com.app.camel;
 
-import com.app.camel.configuration.Configuration;
 import com.app.camel.routes.ProjectRoute;
 import com.app.camel.routes.UserRoute;
 import org.apache.camel.CamelContext;
 import org.apache.camel.impl.DefaultCamelContext;
-import org.apache.log4j.Logger;
 import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.app.camel.configuration.Configuration.*;
 
 public class Application {
 
-    private static final Logger LOGGER = Logger.getLogger(Application.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) throws Exception {
-
         LOGGER.info("Starting application");
 
-        Class.forName(Configuration.DATABASE_DRIVER).newInstance();
+        Class.forName(DATABASE_DRIVER).newInstance();
 
         initializeDatabase();
         CamelContext context = initializeCamel();
@@ -39,15 +40,15 @@ public class Application {
     private static void initializeDatabase() {
         Flyway flyway = new Flyway();
         flyway.setBaselineOnMigrate(true);
-        flyway.setDataSource(Configuration.FLYWAY_URL, Configuration.DATABASE_USER, Configuration.DATABASE_PASSWORD);
-        flyway.setSchemas(Configuration.FLYWAY_SCHEMA_NAME);
+        flyway.setDataSource(FLYWAY_URL, DATABASE_USER, DATABASE_PASSWORD);
+        flyway.setSchemas(FLYWAY_SCHEMA_NAME);
         int migrate = flyway.migrate();
 
         if (migrate > 0) {
-            LOGGER.error("Schema named '" + Configuration.FLYWAY_SCHEMA_NAME + "' or database tables not found");
-            LOGGER.info("Automaticly generated '" + Configuration.FLYWAY_SCHEMA_NAME + "' schema and database tables");
+            LOGGER.error("Schema named '{}' or database tables not found", FLYWAY_SCHEMA_NAME);
+            LOGGER.info("Automaticly generated '{}' schema and database tables", FLYWAY_SCHEMA_NAME);
         } else {
-            LOGGER.info("Schema '" + Configuration.FLYWAY_SCHEMA_NAME + "' is up to date. No migration necessary.");
+            LOGGER.info("Schema '{}' is up to date. No migration necessary.", FLYWAY_SCHEMA_NAME);
         }
     }
 }
