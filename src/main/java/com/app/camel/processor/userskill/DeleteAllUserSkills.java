5e346261330651ce295c9bee82a4f75d88752ec1
Maksym Osmuk <maxim.osmuk@gmail.com>
2016-11-07 12:49:12 +0100

    Delete useless imports and fields

--- src/main/java/com/app/camel/processor/userskill/DeleteAllUserSkills.java
+++ src/main/java/com/app/camel/processor/userskill/DeleteAllUserSkills.java
@@ -4,8 +4,6 @@ import com.app.camel.dao.UserSkillRepository;
 import com.app.camel.dao.impl.UserSkillRepositoryImpl;
 import com.app.camel.util.Precondition;
 import com.google.common.base.Preconditions;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.component.restlet.RestletConstants;
@@ -15,7 +13,6 @@ import org.restlet.data.Status;
 public class DeleteAllUserSkills implements Processor {
 
     private final UserSkillRepository userSkillRepository = new UserSkillRepositoryImpl();
-    private final Gson gson = new GsonBuilder().create();
 
     @Override
     public void process(Exchange exchange) throws Exception {
