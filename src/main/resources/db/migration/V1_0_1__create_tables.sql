c5888fb6bad86015926a692942448e5f3e4052ed
kmaciejewski <kamil.maciejewski@hotmail.com>
2016-11-09 15:26:42 +0100

    Model update

--- src/main/resources/db/migration/V1_0_1__create_tables.sql
+++ src/main/resources/db/migration/V1_0_1__create_tables.sql
@@ -1,79 +1,95 @@
---
--- Table structure for table `position`
---
+
+-- -----------------------------------------------------
+-- Table `library`.`position`
+-- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `position` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `position` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `position` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 13
+DEFAULT CHARACTER SET = utf8;
 
---
--- Table structure for table `user`
---
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `status` varchar(255) NOT NULL,
-  `Position_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_Position_has_Users_Positions_idx` (`Position_id`),
-    CONSTRAINT `fk_Position_has_Users_Positions`
-    FOREIGN KEY (`Position_id`) REFERENCES `position` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `project`
---
+-- -----------------------------------------------------
+-- Table `library`.`project`
+-- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `project` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `project_name` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `project_name` VARCHAR(200) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8;
 
---
--- Table structure for table `user_projects`
---
-CREATE TABLE IF NOT EXISTS `user_projects` (
-  `Users_id` int(11) NOT NULL,
-  `Projects_id` int(11) NOT NULL,
-  PRIMARY KEY (`Users_id`,`Projects_id`),
-  KEY `fk_Users_has_Projects_Projects1_idx` (`Projects_id`),
-  KEY `fk_Users_has_Projects_Users_idx` (`Users_id`),
-  CONSTRAINT `fk_Users_has_Projects_Projects1`
-    FOREIGN KEY (`Projects_id`) REFERENCES `project` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Users_has_Projects_Users`
-    FOREIGN KEY (`Users_id`) REFERENCES `user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `salary`
---
-CREATE TABLE IF NOT EXISTS `salary` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `Users_id` int(11),
-  `Positions_id` int(11),
-  `monthly` int(11) NOT NULL,
-  `per_hour` int(11) NOT NULL,
-  `date_from` date NOT NULL,
-  `date_to` date NOT NULL,
+-- -----------------------------------------------------
+-- Table `library`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(255) NOT NULL,
+  `last_name` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `status` VARCHAR(255) NOT NULL,
+  `Position_id` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_Users_has_salary_idx` (`Users_id`),
-  KEY `fk_Position_has_salary_idx` (`Positions_id`),
+  INDEX `fk_Position_has_Users_Positions_idx` (`Position_id` ASC),
+  CONSTRAINT `fk_Position_has_Users_Positions`
+    FOREIGN KEY (`Position_id`)
+    REFERENCES `library`.`position` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `library`.`salary`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `salary` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `Users_id` INT(11) NULL DEFAULT NULL,
+  `Positions_id` INT(11) NULL DEFAULT NULL,
+  `monthly` INT(11) NOT NULL,
+  `per_hour` INT(11) NOT NULL,
+  `date_from` DATE NOT NULL,
+  `date_to` DATE NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Users_has_salary_idx` (`Users_id` ASC),
+  INDEX `fk_Position_has_salary_idx` (`Positions_id` ASC),
   CONSTRAINT `fk_Position_has_Salaries_Positions`
-    FOREIGN KEY (`Positions_id`) REFERENCES `position` (`id`)
+    FOREIGN KEY (`Positions_id`)
+    REFERENCES `library`.`position` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Users_has_Salaries_Positions`
-    FOREIGN KEY (`Users_id`) REFERENCES `user` (`id`)
+    FOREIGN KEY (`Users_id`)
+    REFERENCES `library`.`user` (`id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
+-- -----------------------------------------------------
+-- Table `library`.`user_projects`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `user_projects` (
+  `Users_id` INT(11) NOT NULL,
+  `Projects_id` INT(11) NOT NULL,
+  PRIMARY KEY (`Users_id`, `Projects_id`),
+  INDEX `fk_Users_has_Projects_Projects1_idx` (`Projects_id` ASC),
+  INDEX `fk_Users_has_Projects_Users_idx` (`Users_id` ASC),
+  CONSTRAINT `fk_Users_has_Projects_Projects1`
+    FOREIGN KEY (`Projects_id`)
+    REFERENCES `library`.`project` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Users_has_Projects_Users`
+    FOREIGN KEY (`Users_id`)
+    REFERENCES `library`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
